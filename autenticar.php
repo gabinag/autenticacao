7e882d14f5d304b3e00a3fa5706ecde6bc2ea5bc
Gabriela Nagamuta <gnagamuta03@gmail.com>
2023-05-16 16:50:35 -0300

    Update style

--- autenticar.php
+++ autenticar.php
@@ -14,12 +14,11 @@ $secret = 'JBSWY3DPEHPK3PXP';
 if(isset($_POST['token'])) {
     $token = $_POST['token'];
     if($g->checkCode($secret, $token)){
-        echo "<p style='color: green;'>Autenticação liberada!</p>";
+        echo "<p style='color: green; font-size: 20px;'>Autenticação liberada!</p>";
     } else {
-        echo "<p style='color: red;'>Token inválido ou expirado</p>";
+        echo "<p style='color: red; font-size: 20px;'>Token inválido ou expirado</p>";
     }
 }
-
 ?>
 
 
@@ -30,62 +29,16 @@ if(isset($_POST['token'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Página de Envio de Código</title>
-	<style>
-		body {
-			background-color: #f2f2f2;
-			font-family: Arial, sans-serif;
-		}
-
-		.container {
-			margin: auto;
-			margin-top: 100px;
-			width: 300px;
-			padding: 20px;
-			background-color: #fff;
-			box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-			border-radius: 5px;
-		}
-
-		.input-group {
-			margin-bottom: 10px;
-		}
-
-		.input-group label {
-			display: block;
-			margin-bottom: 5px;
-			color: #555;
-		}
-
-		.input-group input {
-			width: 92%;
-			padding: 10px;
-			border-radius: 3px;
-			border: 1px solid #ccc;
-		}
-
-		.btn {
-			display: block;
-			background-color: #4CAF50;
-			color: #fff;
-			padding: 10px;
-			border: none;
-			border-radius: 3px;
-			cursor: pointer;
-			width: 100%;
-            font-size: 18px;
-		}
-
-		.btn:hover {
-			background-color: #3e8e41;
-		}
-	</style>
+	<link rel="stylesheet" href="style.css">
+    <title>Página de Autenticação</title>
+	
 </head>
 <body>
     <div class="container">
-		<h2>Enviar Código</h2>
-		<form method="post">
+		<h2>Escaneie o Qr Code:</h2>
+		<form method="post" class="center">
 			<div class="input-group">
+				<img src="<?php echo $g->getUrl('autenticacao-2fatores', 'autenticacao-2fatores.azurewebsites.net', $secret)?>" class="center">
 				<label for="token">Informe o token de autenticação recebido pelo Google Authenticator:</label>
 				<input type="text" name="token" required>
 			</div>
--- verifica-login.php
+++ verifica-login.php
@@ -6,11 +6,11 @@ $senha = $_POST['senha'];
 // Verificar o e-mail e a senha
 if ($email == 'teste@teste.com' && $senha == '1234') {
   // Se as credenciais estiverem corretas, redirecionar para a página de sucesso
-  header('Location: gera-qrcode.php');
+  header('Location: autenticar.php');
   exit;
 } else {
   // Se as credenciais estiverem incorretas, exibir a mensagem de erro
-  echo "<p style='color: red;'>Erro! Senha e/ou e-mail incorreto</p>";
+  echo "<p style='color: red; font-size: 20px;'>Erro! Senha e/ou e-mail incorreto</p>";
 }
 ?>
 
