c43ca778b73bd4c12cf84ef0f18849bbe14b552f
Gabriela Nagamuta <gnagamuta03@gmail.com>
2023-05-26 11:55:00 +0000

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,32 +1,33 @@
-<?php return array(
-    'root' => array(
-        'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'c06a1c4c2fcc935276ab1d454d2ce8c410ec54c5',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'c06a1c4c2fcc935276ab1d454d2ce8c410ec54c5',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'sonata-project/google-authenticator' => array(
-            'pretty_version' => '2.3.1',
-            'version' => '2.3.1.0',
-            'reference' => '71a4189228f93a9662574dc8c65e77ef55061b59',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../sonata-project/google-authenticator',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => '02095c563e3db1cbb586a761208c867ca912eb0c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02095c563e3db1cbb586a761208c867ca912eb0c',
     ),
+    'sonata-project/google-authenticator' => 
+    array (
+      'pretty_version' => '2.3.1',
+      'version' => '2.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '71a4189228f93a9662574dc8c65e77ef55061b59',
+    ),
+  ),
 );
